ebd2d4c01be65bb45fc8117c25ec03fef5caa69e
jjenkins120 <jjenkins120@gmail.com>
2020-08-03 17:16:01 -0500

    Done

--- run.rb
+++ run.rb
@@ -7,31 +7,41 @@ spicy_foods = [
 ]
 
 def print_spicy_foods(spicy_foods)
-
+  spicy_foods.each do |food_hash|
+   puts "#{food_hash[:name]} (#{food_hash[:cuisine]}) | Heat Level: #{food_hash[:heat_level]}"
+  end
 end
 
 def get_names(spicy_foods)
-
+  names_array = spicy_foods.map do |food_hash|
+    food_hash[:name]
+  end
+  names_array
 end
 
 def spiciest_foods(spicy_foods)
-
+  above_five = spicy_foods.select do |food_hash|
+    food_hash[:heat_level] > 5
+  end
+  above_five
 end
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-
+  cuisine_spice = spicy_foods.find do |food_hash|
+    food_hash[:cuisine] == cuisine
+  end
+  cuisine_spice
 end
 
-# BONUS Deliverables
 def print_spiciest_foods(spicy_foods)
-
+  spice_above_five = spiciest_foods(spicy_foods)
+  print_spicy_foods(spice_above_five)
 end
 
 def average_heat_level(spicy_foods)
-
+ heat_level_array = spicy_foods.map do |food_hash|
+  food_hash[:heat_level]
+  end
+  median_heat = (heat_level_array.sum) / (heat_level_array.count)
+  median_heat
 end
-
-# Use this to test your methods
-# run "ruby run.rb" and try calling the methods from the console
-binding.pry
-"pls"
